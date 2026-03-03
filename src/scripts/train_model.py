bc28eca70a79d3433648c71662e57902dc760af0
baddddddddd <eluxeofficial@gmail.com>
2026-03-03 16:03:26 +0800

    Fix rounding error in text grouping

--- src/scripts/train_model.py
+++ src/scripts/train_model.py
@@ -125,14 +125,12 @@ def main(cfg: DictConfig):
 
         concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
         total_length = len(concatenated_examples[list(examples.keys())[0]])
-        total_length = ((total_length + block_size - 1) // block_size) * block_size
 
         result = {
             k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
             for k, t in concatenated_examples.items()
         }
 
-        result["labels"] = result["input_ids"].copy()
         return result
 
     dataset = dataset.map(
